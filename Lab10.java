64b712ad7588f1955102f2489158741bf7584dcb
willfox99 <william_fox@asl.org>
2014-12-10 11:34:45 +0000

    Finished

--- Lab10.java
+++ Lab10.java
@@ -1,23 +1,38 @@
 /**
  * An algorithm helps an Athlete solve a maze, perhaps by keeping one hand against a wall.
  * 
- * @author <...>
- * @version <...>
+ * @author Will Fox
+ * @version 4/ 12 / 14 
  *
  */
 
 import edu.fcps.karel2.Display; 
+import javax.swing.JOptionPane; 
 
 public class Lab10 {
 	 
 	 public static void main(String[] args) {
-		 //TODO Open the map 'maze1' or 'maze2' or 'maze3';
-		 //TODO Set the display to size 10 by 10
-		 Athlete athena = new Athlete(1, 1, Display.NORTH, Display.INFINITY);
+		 		 String number = JOptionPane.showInputDialog("Which maze map? Put number (1, 2 or 3).");
+		 Display.openWorld("maps/maze" + number + ".map");
+		 Display.setSize(10, 10);
+		 Display.setSpeed(10);
+             Athlete athena = new Athlete(1, 1, Display.NORTH, Display.INFINITY);
 		 clearMaze(athena);
 	 }
 	 
 	 public static void clearMaze(Athlete arg) {
-		 //TODO Write an algorithm that directs arg to the beeper at the end of the maze
+    
+		 if(arg.rightIsClear()) {
+            arg.turnRight();
+            arg.move();
+       } else if(arg.frontIsClear()) {
+            arg.move();
+       } else if(arg.leftIsClear()) {
+            arg.turnLeft();
+            arg.move();
+       } else {
+            arg.turnAround();
+            arg.move();
+       }
 	 }
  }
